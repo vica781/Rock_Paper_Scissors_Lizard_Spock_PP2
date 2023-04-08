9cc3e0a60ea4e1a00afc8206e2ac54d0869808c8
Victoria Kalenik <vk781@hotmail.com>
2023-04-08 19:16:12 +0000

    Resolve 'undefined' issue with winning messages for the  computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-// Define an object consisting of five properties representing possible game choices
-// Each property is an array of two elements that can be beaten by the property 
+// Define an object winningCombinations consisting of five properties representing the possible game choices
+// Each property is an array of two elements that can be beaten by the respective property of the winningCombinations object.
+// For example, 'scissors' property has an array containing 'paper' and 'lizard', which can be beaten by 'scissors' in the game.
 const winningCombinations = {
     scissors: ['paper', 'lizard'],
     paper: ['rock', 'spock'],
@@ -8,9 +9,9 @@ const winningCombinations = {
     spock: ['scissors', 'rock'],
 }
 
-// Create an array `pcPicks`
-// Use `for...in` loop to iterate through the keys of winningCombinations object
-// Use push() method to add those keys to the end of the `pcPicks` array
+// Create an array pcPicks containing all possible game choices
+// Use for...in loop to iterate through the keys of winningCombinations object
+// Use push() method to add those keys to the end of the pcPicks array
 const pcPicks = [];
 for (let key in winningCombinations) {
     pcPicks.push(key);
@@ -21,41 +22,55 @@ let playerPick;
 let computerPick;
 let winner;
 
-// Add event listeners to the `buttons` in order to detect which button the player clicked
-// Use `querySelectorAll` to pull all the buttons from the body of index.html
-// Use `forEach` to loop through all obtained buttons
-// Add `click` event listener to set `playerPick` variable to the `data-type` attribute of the button that was clicked
+// Add event listeners to the buttons in order to detect which button the player clicked
+
+// Use querySelectorAll to pull all the buttons from the body of index.html
 const buttons = document.querySelectorAll('.btnChoices');
+
+// Use forEach to loop through all obtained buttons
 buttons.forEach(button => {
+    // Add click event listener to set playerPick variable to the data-type attribute of the button that was clicked
     button.addEventListener('click', function () {
         playerPick = this.dataset.type;
+
         // Generate a random index number to select an element from the `pcPicks` array
         computerPick = pcPicks[Math.floor(Math.random() * pcPicks.length)];
+
+        // Call the `determinWinner()` function to determine the winner of the game
         determinWinner();
+
+        // Call the `updateUI()` function to update the user interface with the winner and game result
         updateUI();
     });
 });
 
-// Write a function to determine a winner based on the comparison 
-// of the player's and computer's choices to the `winningCombinations
-// and assign the result to the `winner` variable
+// This function determines the winner of a game based on the comparison of the player's and computer's choices to the winningCombinations object.
+// It assigns the result to the winner variable and creates a message to display the outcome of the game.
+// The combo variable is used to concatenate the player's and computer's choices and create a key to look up the corresponding outcome message in the outcomeMessages object.
+// If the player and computer choose the same option, the game is a tie.
+// If the player's choice beats the computer's choice, the player wins.
+// If the computer's choice beats the player's choice, the computer wins.
+// The function returns an array with two values: the winner and the message to display the outcome of the game.
 function determinWinner() {
     let winner;
     let message;
+    const combo = playerPick + '-' + computerPick;
     if (playerPick === computerPick) {
         winner = 'tie';
         message = "It's a tie";
     } else if (winningCombinations[playerPick].includes(computerPick)) {
         winner = 'player';
-        message = `${playerPick} ${winningMessages[playerPick][computerPick]} You win!`;
+        message = `${outcomeMessages[combo]} - You win!`;
     } else {
         winner = 'computer';
-        message = `${computerPick} ${winningMessages[computerPick][playerPick]} Computer wins!`;
+        message = `${outcomeMessages[combo]} - Computer wins!`;
     }
     return [winner, message];
 }
 
+// An object containing messages describing the outcome of each possible game result.
 const outcomeMessages = {
+    // Options winning for the player
     'scissors-paper': 'Scissors cuts Paper',
     'paper-rock': 'Paper covers Rock',
     'rock-lizard': 'Rock crushes Lizard',
@@ -65,35 +80,33 @@ const outcomeMessages = {
     'lizard-paper': 'Lizard eats Paper',
     'paper-spock': 'Paper disproves Spock',
     'spock-rock': 'Spock vaporizes Rock',
-    'rock-scissors': 'Rock crushes Scissors'
+    'rock-scissors': 'Rock crushes Scissors',
+  
+    // Options winning for the computer
+    'rock-spock': 'Spock vaporizes Rock',
+    'rock-paper': 'Paper covers Rock',
+    'paper-lizard': 'Lizard eats Paper',
+    'paper-scissors': 'Scissors cuts Paper',
+    'scissors-rock': 'Rock crushes Scissors',
+    'scissors-spock': 'Spock smashes Scissors',
+    'lizard-rock': 'Rock crushes Lizard',
+    'lizard-scissors': 'Scissors decapitates Lizard',
+    'spock-paper': 'Paper disproves Spock',
+    'spock-lizard': 'Lizard poisons Spock',
   };
   
-const winningMessages = {
-    scissors: {
-        paper: 'cuts',
-        lizard: 'decapitates'
-    },
-    paper: {
-        rock: 'covers',
-        spock: 'disproves'
-    },
-    rock: {
-        lizard: 'crushes',
-        scissors: 'crushes'
-    },
-    lizard: {
-        spock: 'poisons',
-        paper: 'eats'
-    },
-    spock: {
-        scissors: 'smashes',
-        rock: 'vaporizes'
-    }
-};
-
-function updateUI() {
+  // Update the UI to reflect the player and computer picks and the game outcome.
+  function updateUI() {
+    // Set the player image to match their pick.
     playerImg.innerHTML = `<i class="far fa-hand-${playerPick}"></i>`;
+  
+    // Set the computer image to match its pick.
     compImg.innerHTML = `<i class="far fa-hand-${computerPick}"></i>`;
+  
+    // Determine the winner and the message to display.
     const [winner, message] = determinWinner();
+  
+    // Set the message to display the outcome.
     result.innerHTML = message;
-}
+  }
+  
