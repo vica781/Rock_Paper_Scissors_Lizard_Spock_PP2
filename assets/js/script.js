3e68e14210dd5f4b041cda73594d70479cb09590
Victoria Kalenik <vk781@hotmail.com>
2023-04-12 14:33:15 +0000

    Change game choice images to ratio coordination

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,11 +104,11 @@ const outcomeMessages = {
 
 //An object containing path to game's images
 const imagePaths = {
-    rock: '../images/rock.webp',
-    paper: '../images/paper_4.webp',
-    scissors: '../images/scissors_1.webp',
-    lizard: '../images/lizard.webp',
-    spock: '../images/spock.webp'
+    rock: '../images/boulder_1.webp',
+    paper: '../images/paper_ball.webp',
+    scissors: '../images/scissors_5.webp',
+    lizard: '../images/lizard_3.webp',
+    spock: '../images/spock_2.webp'
 }
 
 
