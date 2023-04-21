8a33d30c0154bcca91e7184a42345faf7392e444
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-04-21 17:46:59 +0100

    Add back-to-top button to about.html and about.js

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,6 +1,25 @@
-// Add a year to the footer
+// Get the BACK-TO-TOP button
+const backToTopButton = document.getElementById("back-to-top");
+
+// Show the back-to-top button when the player scrolls down from the top of the page
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+// Scroll back to the top of the page after the player clicks the back-to-top button
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// Add a YEAR to the footer
 const currentYear = new Date().getFullYear();
 document.getElementById("current-year").innerHTML =
   "&copy; " +
   currentYear +
   " 'Rock Paper Scissors Lizard Spock' created by Victoria Kalenik";
+
+  
