497ed4cbeec9f8bcad22857f8940826efb5aa9ce
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-04-30 22:59:46 +0100

    Change background images

--- assets/js/script.js
+++ assets/js/script.js
@@ -237,23 +237,20 @@ document.getElementById("reset_btn").addEventListener("click", function () {
 
 // Create an array of image paths for the background options
 const backgroundOptions = [
-  "assets/images/backgrounds/dill.webp",
-  "assets/images/backgrounds/leaf.webp",
-  "assets/images/backgrounds/peacock.webp",
-  "assets/images/backgrounds/rainy_leaf.webp",
-  "assets/images/backgrounds/dandelion.webp",
-
+  "assets/images/backgrounds/blue_feather.webp",
   "assets/images/backgrounds/chinese_lantern.webp",
-  "assets/images/backgrounds/magic_world.webp",
-  "assets/images/backgrounds/mushroom_gills.webp",
-
-  "assets/images/backgrounds/droplets.webp",
-  "assets/images/backgrounds/macro_winter.webp",
-  "assets/images/backgrounds/water_drops.webp",
-
+  "assets/images/backgrounds/dandelion.webp",
+  "assets/images/backgrounds/feather.webp",
+  "assets/images/backgrounds/leaf-1.webp",
+  "assets/images/backgrounds/mushroom.webp",
+  "assets/images/backgrounds/fish_scale_2.webp",
+  "assets/images/backgrounds/fish_scale.webp",
+  "assets/images/backgrounds/leaf.webp",  
+  "assets/images/backgrounds/pettel.webp",
   "assets/images/backgrounds/dandelion_1.webp",
-  "assets/images/backgrounds/poppy_seeds.webp",
-  "assets/images/backgrounds/oyster.webp",
+  "assets/images/backgrounds/succulent.webp",
+  "assets/images/backgrounds/sweet_pea.webp",
+  "assets/images/backgrounds/insect_scales.webp",
 ];
 
 document.body.style.backgroundImage = `url(${backgroundOptions[bgChoice]})`;
